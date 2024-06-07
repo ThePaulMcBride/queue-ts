1ae19bf5df0a7b325e37d6c08db283110d57331f
Paul McBride <hello@paulmcbride.com>
2024-06-07 16:35:45 +0100

    rename stack to queue

--- queue.test.ts
+++ queue.test.ts
@@ -1,38 +1,38 @@
 import { Queue } from "./index";
 
-it("should push an item to the stack", () => {
-  const stack = new Queue();
-  stack.enqueue(1);
-  stack.enqueue(2);
-  expect(stack.tasks).toStrictEqual([1, 2]);
+it("should push an item to the queue", () => {
+  const queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  expect(queue.tasks).toStrictEqual([1, 2]);
 });
 
-it("should pop an item from the stack", () => {
-  const stack = new Queue();
-  stack.enqueue(1);
-  stack.enqueue(2);
-  expect(stack.dequeue()).toBe(1);
-  expect(stack.tasks).toStrictEqual([2]);
+it("should pop an item from the queue", () => {
+  const queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  expect(queue.dequeue()).toBe(1);
+  expect(queue.tasks).toStrictEqual([2]);
 });
 
-it("should peek at the top item from the stack", () => {
-  const stack = new Queue();
-  stack.enqueue(1);
-  stack.enqueue(2);
-  expect(stack.peek()).toBe(1);
-  expect(stack.tasks).toStrictEqual([1, 2]);
+it("should peek at the top item from the queue", () => {
+  const queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  expect(queue.peek()).toBe(1);
+  expect(queue.tasks).toStrictEqual([1, 2]);
 });
 
-it("should return the size of the stack", () => {
-  const stack = new Queue();
-  stack.enqueue(1);
-  stack.enqueue(2);
-  expect(stack.size()).toBe(2);
+it("should return the size of the queue", () => {
+  const queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  expect(queue.size()).toBe(2);
 });
 
-it("should return whether the stack is empty", () => {
-  const stack = new Queue();
-  expect(stack.isEmpty()).toBe(true);
-  stack.enqueue(1);
-  expect(stack.isEmpty()).toBe(false);
+it("should return whether the queue is empty", () => {
+  const queue = new Queue();
+  expect(queue.isEmpty()).toBe(true);
+  queue.enqueue(1);
+  expect(queue.isEmpty()).toBe(false);
 });
